ecc3b2e692be70e3c83fbb051a8594f2c164285e
Taiga ASANO <taiga.asano@mixi.co.jp>
2023-05-31 15:50:17 +0900

    feat: support interpolating hyphen to underscore example: resource id sg-<unique> is interpolating to terraform resource id that is sg_<unique>

--- interpolator/interpolator_test.go
+++ interpolator/interpolator_test.go
@@ -8,32 +8,47 @@ import (
 )
 
 func TestInterpolate(t *testing.T) {
-	i := interpolator.New("azurerm")
-	i.AddResourceAttributes("azurerm_virtual_machine.front", map[string]string{
-		"id":    "secretid",
-		"other": "ovalue",
+
+	t.Run("normal cases", func(t *testing.T) {
+		i := interpolator.New("azurerm")
+		i.AddResourceAttributes("azurerm_virtual_machine.front", map[string]string{
+			"id":    "secretid",
+			"other": "ovalue",
+		})
+		i.AddResourceAttributes("azurerm_virtual_something.front", map[string]string{
+			"id": "secretid",
+		})
+
+		s, ok := i.Interpolate("virtual_machine_id", "secretid")
+		assert.Equal(t, s, "${azurerm_virtual_machine.front.id}")
+		assert.True(t, ok)
+
+		s, ok = i.Interpolate("virtual_machine_potato", "ovalue")
+		assert.Equal(t, s, "${azurerm_virtual_machine.front.other}")
+		assert.True(t, ok)
+
+		s, ok = i.Interpolate("totally_random", "secretid")
+		assert.Equal(t, s, "${azurerm_virtual_something.front.id}")
+		assert.True(t, ok)
+
+		s, ok = i.Interpolate("virtual_id", "secretid")
+		assert.Equal(t, s, "${azurerm_virtual_machine.front.id}")
+		assert.True(t, ok)
+
+		s, ok = i.Interpolate("virtual_machine_potato", "none")
+		assert.Equal(t, s, "")
+		assert.False(t, ok)
+
 	})
-	i.AddResourceAttributes("azurerm_virtual_something.front", map[string]string{
-		"id": "secretid",
+
+	t.Run("resource id is uses hyphen, but but resource is underscore", func(t *testing.T) {
+		aws := interpolator.New("aws")
+		aws.AddResourceAttributes("aws_security_group.sg_test", map[string]string{
+			"id":          "sg-test",
+			"description": "security groups",
+		})
+		s, ok := aws.Interpolate("security_group_id", "sg_test")
+		assert.Equal(t, s, "${aws_security_group.sg_test.id}")
+		assert.True(t, ok)
 	})
-
-	s, ok := i.Interpolate("virtual_machine_id", "secretid")
-	assert.Equal(t, s, "${azurerm_virtual_machine.front.id}")
-	assert.True(t, ok)
-
-	s, ok = i.Interpolate("virtual_machine_potato", "ovalue")
-	assert.Equal(t, s, "${azurerm_virtual_machine.front.other}")
-	assert.True(t, ok)
-
-	s, ok = i.Interpolate("totally_random", "secretid")
-	assert.Equal(t, s, "${azurerm_virtual_something.front.id}")
-	assert.True(t, ok)
-
-	s, ok = i.Interpolate("virtual_id", "secretid")
-	assert.Equal(t, s, "${azurerm_virtual_machine.front.id}")
-	assert.True(t, ok)
-
-	s, ok = i.Interpolate("virtual_machine_potato", "none")
-	assert.Equal(t, s, "")
-	assert.False(t, ok)
 }
